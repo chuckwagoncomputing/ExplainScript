98c512058aec15f294dbf70e2afc9bbf5e9ad8e3
David Holdeman <cook@holdemanenterprises.com>
2013-07-09 19:15:12 -0500

    more progress

--- buildline.sh
+++ buildline.sh
@@ -6,8 +6,33 @@
 # ABOUT    About Info
 # FILE     File in which to save Box
 
+WNAME="Written By $WNAME"
+
 if [ $LIMIT ]; then
-if []
+# look for vars which are longer than the limit once padding and chars are added
+# split them
 end
+
+# find longest
+# find how much spacing is req'd
+# VAR = the variable we are working with
+# PADDING = ($SPACES - (((`echo $CHAR | wc -c')-1) x 2)
+
+center()
+{
+PADDING=$((PADDING-2))
+LINES="$LINES$CHAR"`echo -e $VAR | sed -e :a -e "s/^.\{1,$PADDING\}$/ & /;ta"`
+LEN=`echo $VAR | wc -c`
+LEN=$((LEN-1))
+if [ $((LEN%2)) -eq 0 -a $((PADDING%2)) -eq 0 ]; then
+LINES="$LINES$CHAR"
+elif [ $((LEN%2)) -eq 0 -a $((PADDING%2)) -ne 0 ]; then
+LINES="$LINES $CHAR"
+elif [ $((LEN%2)) -ne 0 -a $((PADDING%2)) -ne 0 ]; then
+LINES="$LINES$CHAR"
+elif [ $((LEN%2)) -ne 0 -a $((PADDING%2)) -eq 0 ]; then
+LINES="$LINES $CHAR"
+fi
+}
 
 LINES="$LINES\n"
