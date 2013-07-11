7f30b5a61c783ccdd61f0630b91b5f7afffa4510
David Holdeman <cook@holdemanenterprises.com>
2013-07-10 19:43:16 -0500

    more function fun

--- buildline.sh
+++ buildline.sh
@@ -8,31 +8,43 @@
 
 WNAME="Written By $WNAME"
 
-if [ $LIMIT ]; then
-# look for vars which are longer than the limit once padding and chars are added
-# split them
-end
-
-# find longest
-# find how much spacing is req'd
-# VAR = the variable we are working with
-# PADDING = ($SPACES - (((`echo $CHAR | wc -c')-1) x 2)
+splitter()
+{
+ for ((i=1;; i++)); do
+  read VAR$i || break;
+ done <<lineToSplit
+`if [ $LIMIT ]; then echo "$@" | fold -sw $LIMIT; else; echo $@; fi`
+lineToSplit
+}
 
 center()
 {
-PADDING=$((PADDING-2))
-LINES="$LINES$CHAR"`echo -e $VAR | sed -e :a -e "s/^.\{1,$PADDING\}$/ & /;ta"`
-LEN=`echo $VAR | wc -c`
-LEN=$((LEN-1))
-if [ $((LEN%2)) -eq 0 -a $((PADDING%2)) -eq 0 ]; then
-LINES="$LINES$CHAR"
-elif [ $((LEN%2)) -eq 0 -a $((PADDING%2)) -ne 0 ]; then
-LINES="$LINES $CHAR"
-elif [ $((LEN%2)) -ne 0 -a $((PADDING%2)) -ne 0 ]; then
-LINES="$LINES$CHAR"
-elif [ $((LEN%2)) -ne 0 -a $((PADDING%2)) -eq 0 ]; then
-LINES="$LINES $CHAR"
-fi
+ PADDING=$((PADDING-2))
+ LINES="$LINES$CHAR"`echo -e $VAR | sed -e :a -e "s/^.\{1,$PADDING\}$/ & /;ta"`
+ LEN=${#VAR}`
+ if [ $((LEN%2)) -eq 0 -a $((PADDING%2)) -eq 0 ]; then
+  LINES="$LINES$CHAR"
+ elif [ $((LEN%2)) -eq 0 -a $((PADDING%2)) -ne 0 ]; then
+  LINES="$LINES $CHAR"
+ elif [ $((LEN%2)) -ne 0 -a $((PADDING%2)) -ne 0 ]; then
+  LINES="$LINES$CHAR"
+ elif [ $((LEN%2)) -ne 0 -a $((PADDING%2)) -eq 0 ]; then
+  LINES="$LINES $CHAR"
+ fi
 }
 
+findlongest()
+{
+LONGEST=0
+for i in $@
+do
+if [ $((`eval echo "$""$i" | wc -c`-1)) -gt $LONGEST ]; then
+LONGEST=$((`eval echo "$""$i" | wc -c`-1))
+LONGESTVAR=$i
+fi
+done
+}
+
+# find how much spacing is req'd
+PADDING=$((SPACES-$((${#VAR}*2))))
 LINES="$LINES\n"
